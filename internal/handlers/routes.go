25efd3894f2d13db13a32a4b72f072b17dce7c23
Nick Quirk <n.quirk87@gmail.com>
2024-04-03 23:17:27 +0100

    cleanup

--- internal/handlers/routes.go
+++ internal/handlers/routes.go
@@ -11,13 +11,12 @@ func GetRoutes(mx *chi.Mux) {
 	mx.Use(middleware.Logger)
 	mx.Use(cors.Handler(cors.Options{
 		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
-		AllowedOrigins: []string{"http://localhost:3000"},
-		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedOrigins:   []string{"http://localhost:3000"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
 		ExposedHeaders:   []string{"Link"},
 		AllowCredentials: false,
-		MaxAge:           300, // Maximum value not ignored by any of major browsers
+		MaxAge:           300, // Maximum value not ignored by any major browsers
 	}))
 
 	// Public Routes
